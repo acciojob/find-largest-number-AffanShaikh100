a927a5685635d99351c426eaf40074897159b6c1
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-14 21:42:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function findLargest(a, b, c) {
   //your code here
-}
+	let arr = [a, b, c];
+	let max = arr[0];
+	for(let i=1;i<arr.length;i++)
+		{
+			if(arr[i]>max)
+			{
+				max = arr[i];
+			}
+		}
+	return max;
+} 
 
 const num1 = parseInt(prompt("Enter First Number."));
 const num2 = parseInt(prompt("Enter Second Number."));
